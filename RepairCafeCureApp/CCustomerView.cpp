6dafeb3f8287e05bcaa583a58d0b9aea1a19634e
artvabasDev <136929352+artvabasDev@users.noreply.github.com>
2023-10-24 23:16:48 +0200

    Starting to add control availability for CCustomView Class

--- RepairCafeCureApp/CCustomerView.cpp
+++ RepairCafeCureApp/CCustomerView.cpp
@@ -13,7 +13,14 @@ IMPLEMENT_DYNCREATE(CCustomerView, CFormView)
 CCustomerView::CCustomerView()
 	: CFormView(IDD_CUSTOMER_FORM)
 	, m_strSearchCustomerSurname(_T(""))
+	, m_strCustomerCellPhone(_T(""))
+	, m_strCustomerComment(_T(""))
+	, m_strCustomerLog(_T(""))
+	, m_strCustomerName(_T(""))
+	, m_strCustomerPhone(_T(""))
+	, m_strCustomerSurname(_T(""))
 {
+
 }
 
 CCustomerView::~CCustomerView()
@@ -28,13 +35,35 @@ void CCustomerView::DoDataExchange(CDataExchange* pDX)
 	DDV_MaxChars(pDX, m_strSearchCustomerSurname, 50);
 	DDX_Control(pDX, IDC_CUSTVIEW_BUTTON_SEARCH, m_btnCustomerSurnameSearch);
 	DDX_Control(pDX, IDC_CUSTVIEW_BUTTON_ADD_NEW_CUSTOMER, m_btnAddNewCustomer);
-	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_LIST, m_ctrExistingCustomerList);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_LIST, m_ctlExistingCustomersList);
+	DDX_Control(pDX, IDC_CUSTVIEW_BUTTON_CUSTOMER_ADD, m_btnAddCustomer);
+	DDX_Control(pDX, IDC_CUSTVIEW_BUTTON_CUSTOMER_ASSETS, m_btnCustomAssets);
+	DDX_Control(pDX, IDC_CUSTVIEW_BUTTON_CUSTOMER_UPDATE, m_btnUpdateCustomer);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_CELLPHONE, m_strCustomerCellPhone);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_COMMENT, m_strCustomerComment);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_LOG, m_strCustomerLog);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_NAME, m_strCustomerName);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_PHONE, m_strCustomerPhone);
+	DDX_Text(pDX, IDC_CUSTVIEW_CUSTOMER_SURNAME, m_strCustomerSurname);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_CELLPHONE, m_ctrCustomerCellPhone);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_COMMENT, m_ctrCustomerComment);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_LOG, m_ctrCustomerLog);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_NAME, m_ctrCustomerName);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_PHONE, m_ctrCustomerPhone);
+	DDX_Control(pDX, IDC_CUSTVIEW_CUSTOMER_SURNAME, m_ctrCustomerSurname);
 }
 
 BEGIN_MESSAGE_MAP(CCustomerView, CFormView)
 	ON_BN_CLICKED(IDC_CUSTVIEW_BUTTON_SEARCH, &CCustomerView::OnClickedCustomViewButtonSearch)
 	ON_EN_CHANGE(IDC_CUSTVIEW_EDITBOX_SURNAME_SEARCH, &CCustomerView::OnChangeCustomViewEditBoxSurnameSearch)
 	ON_WM_UPDATEUISTATE()
+	ON_NOTIFY(NM_DBLCLK, IDC_CUSTVIEW_CUSTOMER_LIST, &CCustomerView::OnDoubleClickCustViewCustomerList)
+	ON_EN_CHANGE(IDC_CUSTVIEW_CUSTOMER_NAME, &CCustomerView::OnChangeCustViewCustomerDetails)
+	ON_EN_CHANGE(IDC_CUSTVIEW_CUSTOMER_SURNAME, &CCustomerView::OnChangeCustViewCustomerDetails)
+	ON_EN_CHANGE(IDC_CUSTVIEW_CUSTOMER_CELLPHONE, &CCustomerView::OnChangeCustViewCustomerDetails)
+	ON_EN_CHANGE(IDC_CUSTVIEW_CUSTOMER_PHONE, &CCustomerView::OnChangeCustViewCustomerDetails)
+	ON_EN_CHANGE(IDC_CUSTVIEW_CUSTOMER_COMMENT, &CCustomerView::OnChangeCustViewCustomerDetails)
+	ON_BN_CLICKED(IDC_CUSTVIEW_BUTTON_ADD_NEW_CUSTOMER, &CCustomerView::OnClickedCustViewButtonAddNewCustomer)
 END_MESSAGE_MAP()
 
 
@@ -89,28 +118,45 @@ BOOL CCustomerView::PreTranslateMessage(MSG* pMsg)
 void CCustomerView::OnClickedCustomViewButtonSearch()
 {
 	UpdateData(TRUE);
-	m_ctrExistingCustomerList.DeleteAllItems();
-	while (m_ctrExistingCustomerList.DeleteColumn(0));
-	m_ctrExistingCustomerList.InsertColumn(0, _T("ID"), LVCFMT_LEFT, 50);
-	m_ctrExistingCustomerList.InsertColumn(1, _T("NAME"), LVCFMT_LEFT, 50);
+	m_btnAddNewCustomer.EnableWindow();
+	m_ctlExistingCustomersList.EnableWindow();
 
-	m_ctrExistingCustomerList.InsertItem(0, _T("1"));
-	m_ctrExistingCustomerList.SetItemText(0, 1, _T("John"));
+	// test data
+	int nIndex = m_ctlExistingCustomersList.InsertItem(0, _T("artvabas"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 1, _T("Arthur"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 2, _T("0636272731"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 3, _T("0725426587"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 4, _T("arthur@domain.com"));
 
-	m_ctrExistingCustomerList.InsertItem(1, _T("2"));
-	m_ctrExistingCustomerList.SetItemText(1, 1, _T("Doe"));
+	nIndex = m_ctlExistingCustomersList.InsertItem(1, _T("Repair cafe"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 1, _T("Cure"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 2, _T("06112112112"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 3, _T("07291191191"));
+	m_ctlExistingCustomersList.SetItemText(nIndex, 4, _T("cafe@domain.com"));
 }
 
 /// <summary>
-/// OnChangeCustomViewEditBoxSurnameSearch is used to enable/disable the search button.
+/// OnChangeCustomViewEditBoxSurnameSearch is used to enable/disable the search button
+/// and the add new customer button. And clear the existing customers list control.
 /// </summary>
 void CCustomerView::OnChangeCustomViewEditBoxSurnameSearch()
 {
 	UpdateData(TRUE);
-	if(m_strSearchCustomerSurname.IsEmpty())
+	if (m_strSearchCustomerSurname.IsEmpty())
+	{
 		m_btnCustomerSurnameSearch.EnableWindow(FALSE);
+		m_btnAddNewCustomer.EnableWindow(FALSE);
+	}
 	else
+	{
+		if(m_ctlExistingCustomersList.GetItemCount() > 0) 
+		{
+			m_ctlExistingCustomersList.DeleteAllItems();
+			m_ctlExistingCustomersList.EnableWindow(FALSE);
+		}
+
 		m_btnCustomerSurnameSearch.EnableWindow();
+	}
 }
 
 /// <summary>
@@ -137,7 +183,23 @@ void CCustomerView::OnUpdateUIState(UINT nAction, UINT nUIElement)
 			{
 				m_btnCustomerSurnameSearch.EnableWindow(FALSE);
 				m_btnAddNewCustomer.EnableWindow(FALSE);
+				m_btnAddCustomer.EnableWindow(FALSE);
+				m_btnCustomAssets.EnableWindow(FALSE);
+				m_btnUpdateCustomer.EnableWindow(FALSE);
+				m_ctrCustomerCellPhone.EnableWindow(FALSE);
+				m_ctrCustomerComment.EnableWindow(FALSE);
+				m_ctrCustomerLog.EnableWindow(FALSE);
+				m_ctrCustomerName.EnableWindow(FALSE);
+				m_ctrCustomerPhone.EnableWindow(FALSE);
+				m_ctrCustomerSurname.EnableWindow(FALSE);
+				m_ctlExistingCustomersList.EnableWindow(FALSE);
 				m_strSearchCustomerSurname.Empty();
+				m_strCustomerCellPhone.Empty();
+				m_strCustomerComment.Empty();
+				m_strCustomerLog.Empty();
+				m_strCustomerName.Empty();
+				m_strCustomerPhone.Empty();
+				m_strCustomerSurname.Empty();
 				UpdateData(FALSE);
 			}
 			break;
@@ -153,4 +215,76 @@ void CCustomerView::OnUpdateUIState(UINT nAction, UINT nUIElement)
 			pChild = pChild->GetWindow(GW_HWNDNEXT);
 			break;
 	}
+}
+
+
+void CCustomerView::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+
+	m_ctlExistingCustomersList.SetExtendedStyle(LVS_EX_FULLROWSELECT | LVS_EX_GRIDLINES);
+
+	m_ctlExistingCustomersList.InsertColumn(0, _T("SURNAME"), LVCFMT_LEFT, 150);
+	m_ctlExistingCustomersList.InsertColumn(1, _T("NAME"), LVCFMT_LEFT, 90);
+	m_ctlExistingCustomersList.InsertColumn(2, _T("CELLPHONE"), LVCFMT_LEFT, 90);
+	m_ctlExistingCustomersList.InsertColumn(3, _T("PHONE"), LVCFMT_LEFT, 90);
+	m_ctlExistingCustomersList.InsertColumn(4, _T("EMAIL"), LVCFMT_LEFT, 150);
+}
+
+
+void CCustomerView::OnDoubleClickCustViewCustomerList(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	LPNMITEMACTIVATE pNMItemActivate = reinterpret_cast<LPNMITEMACTIVATE>(pNMHDR);
+	//  pNMItemActivate->iItem = -1 means no existing item is selected.
+	if (pNMItemActivate->iItem != -1)
+	{
+		m_btnAddCustomer.EnableWindow(FALSE);
+		m_btnCustomerSurnameSearch.EnableWindow(FALSE);
+		m_strSearchCustomerSurname.Empty();
+		
+		m_ctrCustomerCellPhone.EnableWindow();
+		m_ctrCustomerComment.EnableWindow();
+		m_ctrCustomerLog.EnableWindow();
+		m_ctrCustomerName.EnableWindow();
+		m_ctrCustomerPhone.EnableWindow();
+		m_ctrCustomerSurname.EnableWindow();
+
+		UpdateData(FALSE);
+	}
+	*pResult = 0;
+}
+
+
+void CCustomerView::OnChangeCustViewCustomerDetails()
+{
+	UpdateData(TRUE);
+	if (m_strCustomerName.IsEmpty() || m_strCustomerSurname.IsEmpty() ||
+		(m_strCustomerCellPhone.IsEmpty() && m_strCustomerPhone.IsEmpty()))
+	{
+		m_btnAddCustomer.EnableWindow(FALSE);
+		m_btnCustomAssets.EnableWindow(FALSE);
+		m_btnUpdateCustomer.EnableWindow(FALSE);
+	}
+	else
+	{
+		m_btnAddCustomer.EnableWindow();
+		m_btnCustomAssets.EnableWindow();
+		m_btnUpdateCustomer.EnableWindow();
+	}
+}
+
+
+
+void CCustomerView::OnClickedCustViewButtonAddNewCustomer()
+{
+	m_btnAddCustomer.EnableWindow(FALSE);
+	m_btnCustomerSurnameSearch.EnableWindow(FALSE);
+	m_strSearchCustomerSurname.Empty();
+
+	m_ctrCustomerCellPhone.EnableWindow();
+	m_ctrCustomerComment.EnableWindow();
+	m_ctrCustomerLog.EnableWindow();
+	m_ctrCustomerName.EnableWindow();
+	m_ctrCustomerPhone.EnableWindow();
+	m_ctrCustomerSurname.EnableWindow();
 }
--- RepairCafeCureApp/CCustomerView.h
+++ RepairCafeCureApp/CCustomerView.h
@@ -14,37 +14,57 @@
 				DECLARE_DYNCREATE(CCustomerView)
 
 			private:
-				CString m_strSearchCustomerSurname;
+				CListCtrl m_ctlExistingCustomersList;
+				
 				CButton m_btnCustomerSurnameSearch;
 				CButton m_btnAddNewCustomer;
-			public:
-				CMFCShellListCtrl m_ctrExistingCustomerList;
+				CButton m_btnAddCustomer;
+				CButton m_btnCustomAssets;
+				CButton m_btnUpdateCustomer;
+
+				CEdit m_ctrCustomerCellPhone;
+				CEdit m_ctrCustomerComment;
+				CEdit m_ctrCustomerLog;
+				CEdit m_ctrCustomerName;
+				CEdit m_ctrCustomerPhone;
+				CEdit m_ctrCustomerSurname;
+
+				CString m_strSearchCustomerSurname;
+				CString m_strCustomerCellPhone;
+				CString m_strCustomerComment;
+				CString m_strCustomerLog;
+				CString m_strCustomerName;
+				CString m_strCustomerPhone;
+				CString m_strCustomerSurname;
 
 			public:
 				CCustomerView();          
 				virtual ~CCustomerView();
 
-			public:
+			private:
 #ifdef AFX_DESIGN_TIME
 				enum { IDD = IDD_CUSTOMER_FORM };
 #endif
 #ifdef _DEBUG
-				virtual void AssertValid() const;
+				void AssertValid() const override;
 #ifndef _WIN32_WCE
-				virtual void Dump(CDumpContext& dc) const;
+				void Dump(CDumpContext& dc) const override;;
 #endif
 #endif
 			private:
-				virtual BOOL PreTranslateMessage(MSG* pMsg);
-
-			protected:
-				virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV support
+				BOOL PreTranslateMessage(MSG* pMsg) override;
+				void OnInitialUpdate() override;
+				void DoDataExchange(CDataExchange* pDX) override;    // DDX/DDV support
 
 				DECLARE_MESSAGE_MAP()
 			private:
 				afx_msg void OnClickedCustomViewButtonSearch();
 				afx_msg void OnChangeCustomViewEditBoxSurnameSearch();
 				afx_msg void OnUpdateUIState(UINT nAction, UINT nUIElement);
+				afx_msg void OnDoubleClickCustViewCustomerList(NMHDR* pNMHDR, LRESULT* pResult);
+			public:
+				afx_msg void OnChangeCustViewCustomerDetails();
+				afx_msg void OnClickedCustViewButtonAddNewCustomer();
 			};
 		//}
 	//}
